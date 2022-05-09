eb7d518d6850a84de4d7a7cf1a8a093759b771ba
ALI <68592145+Ali-awesome@users.noreply.github.com>
2022-05-09 19:17:47 +0600

    dhsbas

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Adduser from './Components/AddUser/Adduser';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/user' element={<Adduser></Adduser>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/AddUser/Adduser.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Adduser = () => {
+
+    const handleAddUser = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+
+        const user = { name, email }
+
+        fetch('http://localhost:5000/user', {
+            method: "post",
+            headers: {
+                "content-type": 'application/json'
+
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
+
+    return (
+        <div>
+            <h2>Please add a user</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" required />
+                <input type="email" name="email" required />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default Adduser;
